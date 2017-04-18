1fe73b47d5537e5fd2b13ff57d933f870520fb82
Himanshu Bhardwaj <himanshu_bhardwajin@yahoo.co.in>
2017-04-18 20:24:23 +0530

    Unit tests added for FileScanner

--- business/src/test/java/com/himanshu/moviesorganizer/business/filesystem/scanner/FileScannerTest.java
+++ business/src/test/java/com/himanshu/moviesorganizer/business/filesystem/scanner/FileScannerTest.java
@@ -1,5 +1,25 @@
 package com.himanshu.moviesorganizer.business.filesystem.scanner;
 
-public class FileScannerTest {
+import java.io.File;
 
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FileScannerTest {
+  
+  private static FileScanner fScanner;
+  
+  
+  @BeforeClass
+  public static void setup() {
+    fScanner = new FileScanner(FileScannerTest.class.getResource("/").getFile().concat("/test/fs/data"));
+  }
+  
+  @Test
+  public void shouldListAllFileExceptMp3() {
+    File[] files = fScanner.listFiles(".*.(.wmv|.dat)$");
+    Assert.assertThat(files.length, Matchers.equalTo(2));
+  }
 }
